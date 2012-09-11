b67751f6b7dac7839fef9819f27c2838a74d83d6
preinh <preinh@gmail.com>
2012-09-11 12:41:59 -0300

    add some grass funcionalities

--- z_testeGRASS.py
+++ z_testeGRASS.py
@@ -37,6 +37,65 @@ class grass_app(object):
             
         r = grass.read_command("g.region", flags='p' )
         print r
+        
+        
+        bsb_shp="/Users/pirchiner/Documents/ws_indigo/MMD_TFinal/resources/gis_data/bsb_br.shp"
+        bsb_grass="test_in_ogr"
+        grass.run_command("v.in.ogr", 
+                          dsn=bsb_shp, 
+                          output=bsb_grass,
+                          #layer="1",
+                          type="point",
+                          overwrite=True)
+
+        faults_shp="/Users/pirchiner/Documents/ws_indigo/MMD_TFinal/resources/gis_data/faults_semNE.shp"
+        faults_grass="faults_semNE_grass"
+        grass.run_command("v.in.ogr", 
+                          dsn=faults_shp,
+                          output=faults_grass,
+                          #layer="1",
+                          type="line",
+                          overwrite=True)
+        
+        distances_grass = "tmp_distances3"
+        grass.run_command("v.distance",
+                          _from=bsb_grass, 
+                          to=faults_grass, 
+                          output=distances_grass+"_raw", 
+                          overwrite=True, 
+                          upload="cat", 
+                          column="cat")
+
+        grass.run_command("v.category", 
+                          input=distances_grass+"_raw", 
+                          output=distances_grass,
+                          #layer=1, 
+                          type="line", 
+                          op="add",
+                          overwrite=True)
+
+        grass.run_command("v.db.addtable", 
+                          map=distances_grass,
+                          #table=distances_grass+"_table",
+                          column="dist DOUBLE",
+                          #layer=1,
+                          overwrite=True)
+#        grass.run_command("v.db.connect", 
+#                          driver="sqlite",
+#                          map=distances_grass,
+#                          )
+        print "bla"
+        grass.run_command("v.to.db", 
+                          map=distances_grass, 
+                          option="length", 
+                          column="dist",
+                          #layer=1,
+                          overwrite=True)
+
+        grass.run_command("v.univar", 
+                          map=distances_grass, 
+                          column="dist")
+
 
 if __name__ == '__main__':
     app = grass_app()
